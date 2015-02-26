04af7bfba46cf46638bf1a378646366326d139d4
DJ Misurelli <deeger@DMisurelli-MBP.ad.uvu.edu>
2015-02-26 14:03:54 -0700

    /post page will now display a user name and an email when a proper post feeds key pair values

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'DJs Amazing Page' });
 });
 
+// accept POST request on the homepage
+router.post('/post', function (req, res, next) {
+  res.render('post',{user :req.body.userName, email: req.body.email});
+});
+
 module.exports = router;
